1acc0a69c09d353e7e26a18ea46ee256bec29641
Ohmatt160 <mattwhingan@gmail.com>
2025-08-10 18:58:03 +0100

    commit

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your-jwt-secret'
